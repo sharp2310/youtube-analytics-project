6ef58bc5a5147249864b27abf6878e70c3000097
ilya <ilyamalahov038@gmail.com>
2024-01-19 12:05:20 +0300

    add

---
+++ config.py
@@ -0,0 +1,5 @@
+import os.path
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+CHANNEL_DATA = os.path.join(ROOT_DIR, 'Data', 'moscowpython.json')
--- src/channel.py
+++ src/channel.py
@@ -1,6 +1,8 @@
 import json
 import os
+
 from googleapiclient.discovery import build
+from config import CHANNEL_DATA
 
 class Channel:
     """Класс для ютуб-канала"""
@@ -14,9 +16,39 @@ class Channel:
         self.__channel_id = channel_id
         self.channel = self.youtube.channel().list(id=self.__channel_id, part='snippet,statistics').execute()
 
+        self.title = self.channel_data['items'][0]['snippet']['title']
+        self.description = self.channel_data['items'][0]['snippet']['description']
+        self.url = f"https://www.youtube.com/channel/{self.__channel_id}"
+
+        self.subs_count = self.channel_data['items'][0]['statistics']['subscriberCount']
+        self.video_count = self.channel_data['items'][0]['statistics']['videoCount']
+        self.views_count = self.channel_data['items'][0]['statistics']['subscriberCount']
+
+        self.new_data = {
+            'id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subs_count': self.subs_count,
+            'video_count': self.video_count,
+            'views_count': self.views_count,
+        }
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
 
         print(self.channel)
 
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, file_name):
+        json_data = json.dumps(self.new_data, ensure_ascii=False)
+
+        with open(f"../Data/{file_name}", 'w', encoding='windows-1251') as file:
+            file.write(json_data)
